43853174319075603c1ec821bbd7e9971f6c1bf2
DayanaNur <154298960+DayanaNur@users.noreply.github.com>
2024-04-04 12:35:27 +0600

    commit3

---
+++ src/ReverseOrder.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ReverseOrder {
+    public static void reverse(int[] arr , int start , int end){
+        if (start >= end)
+            return;
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverse(arr , start + 1 , end - 1);
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println();
+        for(int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        reverse(arr , 0 , n-1);
+        System.out.println();
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+
+        }
+        scanner.close();
+    }
+
+}

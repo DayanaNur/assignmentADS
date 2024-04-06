9dfe7c9bab002fc2e0887051de395071441d24ae
DayanaNur <154298960+DayanaNur@users.noreply.github.com>
2024-04-06 16:44:00 +0600

    add comments

--- src/BinomialCoefficient.java
+++ src/BinomialCoefficient.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class BinomialCoefficient {
+    /**
+     * this method calculates the binomial coefficient recursively using the formula:
+     * C(n, k) = C(n - 1, k - 1) + C(n - 1, k)
+     * @param n The total number of items.
+     * @param k The number of items to choose.
+     * @return The binomial coefficient C(n, k).
+     */
     public static int binomialCoefficient(int n , int k){
+        // if k equals 0 or k equals n, return 1
         if ( k == 0 || k == n){
             return 1;
         }
